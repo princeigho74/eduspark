38d0176ca46f3489df75c84daa2c8d2ffd4f3967
Happy Igho Umukoro <princeigho74@gmail.com>
2025-10-26 16:08:50 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,29 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
 })
-```
-
-### **Step 2: Make sure `.gitignore` is separate**
-Create/update `.gitignore` file:
-```
-node_modules/
-.pnp
-.pnp.js
-coverage/
-build/
-dist/
-.DS_Store
-.env
-.env.local
-.env.development.local
-.env.test.local
-.env.production.local
-npm-debug.log*
-yarn-debug.log*
-yarn-error.log*
-.vscode/
-.idea/
